a7fb3e60c10bcc6145ee8a963a030a0a1b8ca3ef
Alexander <32470113+dojekon@users.noreply.github.com>
2022-11-13 09:23:19 +0300

    All tasks finished

--- lab2/src/Main.java
+++ lab2/src/Main.java
@@ -1,3 +1,6 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -18,27 +21,52 @@ public class Main {
             System.out.println(e.toString());
             System.exit(-1);
         }
-
-        int[][] arr = Matrix.randomSquareMatrix(N,-N,N);
-        Matrix.printMatrix(arr);
-        System.out.println();
-        try {
-            // 90
-            arr = Matrix.turnCounterClockwise90(arr);
-            Matrix.multiplyMatrix(arr);
-            Matrix.printMatrix(arr);
-            // 180
-            arr = Matrix.turnCounterClockwise180(arr);
-            Matrix.multiplyMatrix(arr);
-            Matrix.printMatrix(arr);
-            // 270
-            arr = Matrix.turnCounterClockwise270(arr);
-            Matrix.multiplyMatrix(arr);
-            Matrix.printMatrix(arr);
-        } catch (Exception e) {
-            System.out.println(e.toString());
-            System.exit(-1);
+        String output;
+       while(true) {
+           int[][] arr = Matrix.randomSquareMatrix(N,-N,N);
+           /*
+           Matrix.printMatrix(arr);
+           System.out.println();
+           */
+           output = Arrays.deepToString(arr);
+           try {
+               // 90
+               arr = Matrix.turnCounterClockwise90(arr);
+               Matrix.multiplyMatrix(arr);
+               /*
+               System.out.println("Поворот на 90 и деление");
+               Matrix.printMatrix(arr);
+               System.out.println();
+                */
+               output += "\nПоворот на 90 и деление:\n" + Arrays.deepToString(arr);
+               // 180
+               arr = Matrix.turnCounterClockwise180(arr);
+               Matrix.multiplyMatrix(arr);
+               /*
+               System.out.println("Поворот на 180 и деление");
+               Matrix.printMatrix(arr);
+               System.out.println();
+                */
+               output += "\nПоворот на 180 и деление:\n" + Arrays.deepToString(arr);
+               // 270
+               arr = Matrix.turnCounterClockwise270(arr);
+               Matrix.multiplyMatrix(arr);
+               /*
+               System.out.println("Поворот на 270 и деление");
+               Matrix.printMatrix(arr);
+               System.out.println();
+                */
+               output += "\nПоворот на 270 и деление:\n" + Arrays.deepToString(arr);
+               break;
+           } catch (Exception e) {
+               System.out.println(e.toString());
+               continue;
+           }
+       }
+        try (PrintWriter out = new PrintWriter("output.txt")) {
+            out.println(output);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
-        Matrix.printMatrix(arr);
     }
 }
