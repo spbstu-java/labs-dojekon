c9f0c4a51cdbcdbc1e1770c4d7a13fa41542436e
Alexander <32470113+dojekon@users.noreply.github.com>
2022-12-06 23:29:37 +0300

    Add task1 and task2 methods

--- lab5/src/Methods.java
+++ lab5/src/Methods.java
@@ -1,2 +1,17 @@
+import java.util.List;
+
 public class Methods {
+    public static double task1(List<Integer> list) {
+        return list.stream()
+                .mapToInt(Integer::intValue)
+                .summaryStatistics()
+                .getAverage();
+    }
+
+    public static List<String> task2(List<String> list) {
+        return list.stream()
+                .map(str -> str = str.toUpperCase())
+                .map(str -> str = "_new_" + str)
+                .toList();
+    }
 }
