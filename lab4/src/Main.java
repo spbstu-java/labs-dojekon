eafffd233d62da74e836d59a86be1bc752602276
Alexander <32470113+dojekon@users.noreply.github.com>
2022-12-06 23:05:43 +0300

    Update Main.java

--- lab4/src/Main.java
+++ lab4/src/Main.java
@@ -1,5 +1,26 @@
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        for (Method m : PrivateMethods.class.getDeclaredMethods()) {
+            if (m.isAnnotationPresent(IntAnnotation.class)) {
+                IntAnnotation annotation = m.getAnnotation(IntAnnotation.class);
+                m.setAccessible(true);
+                for (int j = 0; j < annotation.value(); j++) {
+                    try {
+                        if (m.getParameterCount() == 2) {
+                            m.invoke(null, j + 1, j + 1);
+                        } else {
+                            m.invoke(null);
+                        }
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
     }
 }
